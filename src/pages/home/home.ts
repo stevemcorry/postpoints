be238f6d39af520680e6b0eff441f064663e5efc
Stephen Corry <stevemcorry@gmail.com>
2017-03-22 16:27:18 -0600

    home style

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,5 +11,13 @@ export class Home {
   constructor(public navCtrl: NavController) {
     
   }
-
+  popular = [1,2,3]
+  categories = [
+    {name: "Food"},
+    {name: "Fun & Fitness"},
+    {name: "Shopping"},
+    {name: "Online"},
+    {name: "More"},
+    {name: "Map"}
+  ]
 }
