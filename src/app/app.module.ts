26295d7ea4a3914588d293bae045928a638db7ef
Stephen Corry <stevemcorry@gmail.com>
2017-04-12 21:25:57 -0600

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+// Pages
 import { MyApp } from './app.component';
 import { Home } from '../pages/home/home';
 import { Saved } from '../pages/saved/saved';
@@ -8,30 +10,54 @@ import { Refer } from '../pages/refer/refer';
 import { Contact } from '../pages/contact/contact';
 import { Help } from '../pages/help/help';
 
+//Modals
+import { Category } from '../modals/category/category';
 
+
+// ANGULAR FIRE
+import { AngularFireModule } from 'angularfire2';
+export const firebaseConfig = {
+    apiKey: "AIzaSyASGQLdYEPXziFCFjavt04Ik06hPlwTtmo",
+    authDomain: "coupost-7fc1b.firebaseapp.com",
+    databaseURL: "https://coupost-7fc1b.firebaseio.com",
+    projectId: "coupost-7fc1b",
+    storageBucket: "coupost-7fc1b.appspot.com",
+    messagingSenderId: "852600373661"
+  };
 
 @NgModule({
   declarations: [
     MyApp,
+
+    //Pages
     Home,
     Saved,
     Free,
     Refer,
     Contact,
     Help,
+
+    //Modals
+    Category,
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+
+    // PAGES
     Home,
     Saved,
     Free,
     Refer,
     Contact,
     Help,
+
+    //Modals
+    Category,
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/modals/category/category.ts
+++ src/modals/category/category.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, NavParams } from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
-import { Category } from '../../modals/category/category';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-category',
+  templateUrl: 'category.html'
 })
-export class Home {
+export class Category {
   categories: FirebaseListObservable<any>;
-  constructor(public navCtrl: NavController, public angFire: AngularFire, public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public angFire: AngularFire, public alertCtrl: AlertController, public params: NavParams) {
     this.categories = angFire.database.list('/Categories');
+    this.category = this.params.get('category')
   }
+  category;
   popular = [1,2,3];
   add(){
     let prompt = this.alertCtrl.create({
